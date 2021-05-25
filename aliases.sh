9a465246cc12dfc2c044932d26b608d2c08e38ab
Paras Sanghavi <paras@disambiguo.us>
2021-05-25 14:40:05 -0700

    extract aliases

---
+++ aliases.sh
@@ -0,0 +1,8 @@
+# Aliases
+alias g=git
+alias gst='git status'
+alias gp='git push'
+alias gd='git diff'
+alias wip='git wip'
+alias pr='git push -u origin head && hub compare'
+alias pull='git pull && git cleanup'
--- config.fish
+++ config.fish
@@ -5,15 +5,8 @@ export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
 export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
 
-# Aliases
-alias g=git
-alias gst='git status'
-alias gp='git push'
-alias gd='git diff'
-alias wip='git wip'
-alias pr='git push -u origin head && hub compare'
-alias pull='git pull && git cleanup'
 set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 
+source aliases.sh
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
